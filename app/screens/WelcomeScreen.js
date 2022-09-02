8bc3e03fabf115eef76215baba47406581f5d624
Dthakkar18 <91099584+Dthakkar18@users.noreply.github.com>
2022-09-01 19:05:37 -0500

    Created front page with title and button

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,10 +1,19 @@
 import React from 'react';
-import { Text, ImageBackground, StyleSheet, View, SafeAreaView} from 'react-native';
+import { Text, ImageBackground, StyleSheet, View, SafeAreaView, Button} from 'react-native';
 
 function WelcomeScreen(props) {
     return (
-        <ImageBackground style={styles.background} source={require("../assets/nightBackground.jpg")}>
-            
+        <ImageBackground style={styles.background} source={require("../assets/kitchenBackground.jpg")}>
+            <SafeAreaView style={styles.titleView}>
+                <Text style={styles.text}>Ck's Kitchen</Text>
+            </SafeAreaView>
+            <SafeAreaView style={styles.buttonView}>
+                <Button
+                    title="Place Order"
+                    color="white"
+                    onPress={() => alert("Place Order Clicked!!")}
+                />
+            </SafeAreaView>
         </ImageBackground>
     );
 }
@@ -12,6 +21,33 @@ function WelcomeScreen(props) {
 const styles = StyleSheet.create({
     background: {
         flex: 1,
+    },
+    titleView: {
+        position: "absolute",
+        top: 200,
+        bottom: 0,
+        right: 0,
+        left: 0,
+        textAlign: "center",
+        alignItems: "center"
+    },
+    buttonView: {
+        position: "absolute",
+        top: 450,
+        bottom: 355,
+        right: 120,
+        left: 120,
+        textAlign: "center",
+        alignItems: "center",
+        backgroundColor: "black",
+        borderRadius: 10
+    },
+    text: {
+        fontFamily: "Arial",
+        fontSize: 30,
+        fontWeight: "bold"
+    },
+    button: {
 
     }
 })
